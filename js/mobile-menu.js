462b1aca6c522a5c771ebf9cf1e3d2ca26c528d4
IgorJurievic <elfenomen@ukr.net>
2022-11-23 16:05:52 +0200

    add-mob-tablet-desktop-versions

---
+++ js/mobile-menu.js
@@ -0,0 +1,22 @@
+(() => {
+  const refs = {
+    openMenulBtn: document.querySelector("[data-menu-open]"),
+    closeMenuBtn: document.querySelector("[data-menu-close]"),
+    menu: document.querySelector("[data-menu]"),
+    body: document.querySelector("[body]"),
+    menuList: document.querySelector(".mob-menu-list"),
+  };
+
+  refs.openMenulBtn.addEventListener("click", toggleMenu);
+  refs.closeMenuBtn.addEventListener("click", toggleMenu);
+  refs.menuList.addEventListener("click", removeMenu);
+
+  function toggleMenu() {
+    refs.menu.classList.toggle("is-hidden");
+    refs.body.classList.toggle("no-skroll");
+  }
+  function removeMenu() {
+    refs.menu.classList.add("is-hidden");
+    refs.body.classList.remove("no-skroll");
+  }
+})();
